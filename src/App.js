94d7fc393f1b3260b8e9e51966d14e5cf6aac875
agarwarahul-22 <ag.rahul223@gmail.com>
2021-06-26 15:01:41 +0530

    OrderStats and OrderReq

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Orderstats from "./components/OrderStats/orderstats";
+import Orderrequest from "./components/OrderRequest/orderrequest";
+import Orderstatus from "./components/OrderStatus/orderstatus";
 
 function App() {
     return (
         <div>
-            <h1>Hey Rahul!</h1>
+          <Orderstats/>
+          <Orderrequest />
+          <Orderstatus/>
         </div>
     );
 }
---
+++ src/components/OrderStatus/orderstatus.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Box = styled.div`
+  background-color: white;
+  height: 200px;
+  float: left;
+`;
+function Orderstatus() {
+  return (
+    <Box>
+    </Box>
+  )
+}
+
+export default Orderstatus
