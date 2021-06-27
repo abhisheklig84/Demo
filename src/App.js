72c19d7dd2203bc97d12ebe4d3d13cdb7e7b9387
abhisheklig84 <39373032+abhisheklig84@users.noreply.github.com>
2021-06-27 12:58:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div>
-            <h1>Hey Abhishek!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
-
 export default App;
